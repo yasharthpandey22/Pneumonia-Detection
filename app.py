7516f5f108447aabf46f7e7b9b9422d47df57116
Yasharth Pandey <yasharthpandey223@gmail.com>
2026-08-05 16:53:31 +0530

    Fix Streamlit image display

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ if uploaded_file is not None:
 
     image = Image.open(uploaded_file).convert("RGB")
 
-    st.image(image, caption="Uploaded Image", use_container_width=True)
+    st.image(image, caption="Uploaded Image", width=400)
 
     image = image.resize((224, 224))
 
